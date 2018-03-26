bb5233e17abb7fbfa62524df9a18e4911bcd71e9
Luis Robles <lfr4704@gmail.com>
2018-03-26 19:24:28 +0000

    revised

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,2 +1,7 @@
-const myLibrary = require("./scripts1.js")
-myLibrary.print("hello");
+const myLibrary = require("./scripts1.js")  //this is an absolute path. Node does not understand relative path 
+
+//this code calls out the functions to be run from the script1 file
+console.log(myLibrary.sum2(10,20));
+console.log(myLibrary.getDate());
+console.log(myLibrary.printName("hello"));
+console.log(myLibrary.changeStringToNum());
--- scripts/scripts1.js
+++ scripts/scripts1.js
@@ -2,25 +2,28 @@
 
 function getDate() {
 var today = new Date();
-console.log(today);
 return today;
 }
 
 function getSumProduct (num1, num2) {
     var sum = num1 + num2;
-    var product = num1 * num2
-    console.log("enter numbers")
+    var product = num1 * num2;
     return sum + product;
 }
 
-function printName () {
-    console.log("Luis Robles");
+function printName (text) {
+    return  text + " World";
 }
 
 function changeStringToNum () {
     var num3 = parseInt("10");
-    console.log(num3);
     return num3;
 }
 
-console.log(printName());
+//this code tell what functions are allowed to run in this file.
+module.exports = { 
+    getDate, //you dont have to assign a name
+    sum2: getSumProduct, // to the left of ":" can be called differently or the same
+    printName: printName,
+    changeStringToNum: changeStringToNum
+}
