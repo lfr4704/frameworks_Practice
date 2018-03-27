7530fbf6836892e256e19dc84faecdde0a2d87cb
Luis <lfr4704@gmail.com>
2018-03-27 03:28:29 -0700

    added files

---
+++ scripts/demo.js
@@ -0,0 +1,3 @@
+let _ = require('lodash'); //common practice to name it with underscore. no default loction is required since node knows its default location for its packages
+
+console.log(_.random(1,1000)); //another package is nodemon which tracks changes automatically and you do not have to write node everytime to run it in terminal 
